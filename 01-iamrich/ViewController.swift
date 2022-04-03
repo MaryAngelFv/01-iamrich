5b1a73226281a9e66339104d482413ba5bce5f75
MaryAngelFv <90918206+MaryAngelFv@users.noreply.github.com>
2022-04-03 18:53:46 -0500

    creating i am rich app

--- 01-iamrich/ViewController.swift
+++ 01-iamrich/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var diamondImage: UIImageView!
+    
+    @IBOutlet weak var clickButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func showText(_ sender: UIButton) {
+        
+       let controller = UIAlertController(
+        title: "I am Rich!",
+        message: """
+            I am rich
+            I diserve it
+            I am good
+            healthy and successfull
+            """,
+        preferredStyle: .alert
+       )
+        
+        let action = UIAlertAction(title: "Aceptar", style: .default) { action in
+            print("He oprimido aceptar")
+        }
+        
+        let action2 = UIAlertAction(title: "Cancelar", style: .cancel) { _ in
+            print("He oprimido cancelar")
+        }
+        
+        let action3 = UIAlertAction(title: "Borrar", style: .destructive) { _ in
+            print("He oprimido borrar")
+        }
+        
+        controller.addAction(action)
+        
+        controller.addAction(action2)
+        
+        controller.addAction(action3)
+        
+        show(controller, sender: nil)
+    }
+    
+    
+    
+    
 }
 
